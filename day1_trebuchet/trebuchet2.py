d6b966cb4eba7f65d10ca76d57b12a91acd1b1f2
Chris Smith <cds30895@gmail.com>
2023-12-03 09:43:47 -0800

    Day1Puzzle2: Function to replace alphas with nums

--- day1_trebuchet/trebuchet2.py
+++ day1_trebuchet/trebuchet2.py
@@ -6,7 +6,15 @@ Description: This code will take alphanumeric input and parse the numbers from e
     item and give the sum of all items.
 """
 
+# Function to replace alpha representations of numbers with their numeric counterparts
+def replace_alpha_with_num(item, written_numbers, nums):
+    for i, alphanum in enumerate(written_numbers):
+        item.replace(alphanum, nums[i])
+
 # Open the file and place each line in a list
 with open('day1_trebuchet/puzzle_input1.txt', 'r') as file:
     raw_data = file.read()
     data = raw_data.split('\n')
+
+written_numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+nums = range(1, 9)
