8cf94bca9d6c4376c7c8d7fdaf37136cd770afc1
Chris Smith <cds30895@gmail.com>
2023-12-04 07:39:51 -0800

    Added function to split each round into list of colors

--- day2_cube_conundrum/cube1.py
+++ day2_cube_conundrum/cube1.py
@@ -6,14 +6,24 @@ Description: Given a game in which cubes are drawn from a bag with an unknown nu
     green and 14 blue cubes.
 """
 
-# Function to split list by semicolons
+#### FUNCTIONS ####
+
+# Function to split game into rounds by semicolons, slicing past the game number
 def split_games_into_rounds(game):
     rounds = game[(game.index(':') + 2):].split('; ')
     return rounds
+
+# Function to split rounds into colors
+def split_rounds_into_colors(round):
+    colors = round.split(', ')
+    return colors
+
+
+#### MAIN CODE ####
 
 # Open the file and place each line in a list
 with open('day2_cube_conundrum/puzzle_input2.txt', 'r') as file:
     raw_data = file.read()
     data = raw_data.split('\n')
 
-print(split_games_into_rounds(data[3]))
+print(split_games_into_rounds(data[3])[1].split(', '))
