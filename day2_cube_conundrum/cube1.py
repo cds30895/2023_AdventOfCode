912fc98a58ce49ba4142b8520c9a7a1d1c5cba16
Chris Smith <cds30895@gmail.com>
2023-12-04 07:34:05 -0800

    Added function to split games into rounds

--- day2_cube_conundrum/cube1.py
+++ day2_cube_conundrum/cube1.py
@@ -1,11 +1,19 @@
 """
 Advent of Code Day 2: Cube Conundrum - Puzzle 1
 Author: Chris Smith
-Description: 
+Description: Given a game in which cubes are drawn from a bag with an unknown number of cubes,
+    this program will determine which games are possible with the given numbers of 12 red, 13
+    green and 14 blue cubes.
 """
 
+# Function to split list by semicolons
+def split_games_into_rounds(game):
+    rounds = game[(game.index(':') + 2):].split('; ')
+    return rounds
 
 # Open the file and place each line in a list
 with open('day2_cube_conundrum/puzzle_input2.txt', 'r') as file:
     raw_data = file.read()
     data = raw_data.split('\n')
+
+print(split_games_into_rounds(data[3]))
