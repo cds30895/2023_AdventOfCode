9f9c081656668fd856ba1d178077713262bcf195
Chris Smith <cds30895@gmail.com>
2023-12-04 17:25:53 -0800

    Added classes for doubly linked list, added each line as new node

--- day3_gear_ratios/gears1.py
+++ day3_gear_ratios/gears1.py
@@ -5,6 +5,36 @@ Description: This program will take a large input of numbers separated by period
     the sum of all numbers that are adjacent to a symbol horizontally, vertically, or diagonally.
 """
 
+#### CLASSES ####
+
+class DoubleNode():
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, value):
+        new_node = DoubleNode(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            print(f'Head Node created: {self.head.value}')
+            return
+        
+        new_node.prev = self.tail
+        self.tail.next = new_node
+        self.tail = new_node
+        print(f'Appended new Node with value: {self.tail.value}')
+
+    
+
+
 #### FUNCTIONS ####
 
 
@@ -12,7 +42,12 @@ Description: This program will take a large input of numbers separated by period
 
 #### MAIN CODE ####
 
+dllist = DoublyLinkedList()
+
 # Open the file and place each line in a list
 with open('day3_gear_ratios/puzzle_input3.txt', 'r') as file:
     raw_data = file.read()
     data = raw_data.split('\n')
+
+for line in data:
+    dllist.append(line)
