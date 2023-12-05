feaf096eeedd8dfb228606afe36c9bda9ba7a205
Chris Smith <cds30895@gmail.com>
2023-12-04 16:07:19 -0800

    Copied functions from cube1.py to cube2.py

---
+++ day2_cube_conundrum/cube2.py
@@ -0,0 +1,56 @@
+"""
+Advent of Code Day 2: Cube Conundrum - Puzzle 2
+Author: Chris Smith
+Description: Given a game in which cubes are drawn from a bag with an unknown number of cubes,
+    this program will determine the minimum number of each color of cube for each game.  These
+    minimums will be multiplied to calculate each games "power" and summed to find the total.
+"""
+
+#### FUNCTIONS ####
+
+# Function to split game into rounds by semicolons, slicing past the game number
+def split_games_into_rounds(game):
+    rounds = game[(game.index(':') + 2):].split('; ')
+    return rounds
+
+# Function to split rounds into colors
+def split_rounds_into_colors(round):
+    colors = round.split(', ')
+    return colors
+
+# Function to return numbers associated with each color
+def count_colors(colors):
+    color_count = {'red': 0, 'green': 0, 'blue': 0}
+    for color in colors:
+        if 'red' in color:
+            red = ''
+            for i in color:
+                if i.isnumeric():
+                    red += i
+            color_count['red'] = int(red)
+
+        if 'green' in color:
+            green = ''
+            for i in color:
+                if i.isnumeric():
+                    green += i
+            color_count['green'] = int(green)
+
+        if 'blue' in color:
+            blue = ''
+            for i in color:
+                if i.isnumeric():
+                    blue += i
+            color_count['blue'] = int(blue)
+
+    return color_count
+
+
+#### MAIN CODE ####
+
+total = 0
+
+# Open the file and place each line in a list
+with open('day2_cube_conundrum/puzzle_input2.txt', 'r') as file:
+    raw_data = file.read()
+    data = raw_data.split('\n')
