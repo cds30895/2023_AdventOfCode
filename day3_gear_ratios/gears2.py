aabf529e569f3522013d68274624342fe060c489
Chris Smith <cds30895@gmail.com>
2023-12-22 11:07:43 -0800

    Added DoublyLinkedList class

--- day3_gear_ratios/gears2.py
+++ day3_gear_ratios/gears2.py
@@ -61,3 +61,21 @@ class DoubleNode():
                 gears_idx.append(i)
 
         return gears_idx
+    
+
+class DoublyLinkedList():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, value):
+        new_node = DoubleNode(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            return
+        
+        new_node.prev = self.tail
+        self.tail.next = new_node
+        self.tail = new_node
